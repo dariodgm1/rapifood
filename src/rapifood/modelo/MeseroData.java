143de32789564d46f208217f17576fa884c880a1
Usuario <ciro2412.dm@gmail.com>
2020-11-06 01:20:48 -0300

    agregando clasedate Mesero

--- src/rapifood/modelo/MeseroData.java
+++ src/rapifood/modelo/MeseroData.java
@@ -6,6 +6,14 @@
 package rapifood.modelo;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import rapifood.entidades.Mesero;
 
 /**
  *
@@ -21,4 +29,58 @@ public class MeseroData {
     
     }  
     
+    public void guardarMesero(Mesero mesero){
+        
+    
+        String sql="INSERT into mesero (idMesero, dniMesero, cuitMesero, nombreMesero, apellidoMesero, activo) "
+                + "VALUES(?, ?, ?, ?, ?, ?);";
+        
+        try{
+        
+        PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        ps.setInt(1,mesero.getIdMesero());
+        ps.setInt(2,mesero.getDniMesero() );
+        ps.setInt(3, mesero.getCuitMesero());
+        ps.setString(4, mesero.getNombreMesero());
+        ps.setString(5, mesero.getApellidoMesero());
+        ps.setBoolean(6, mesero.isActivo());
+        
+        ps.executeUpdate();
+        
+        ResultSet rs = ps.getGeneratedKeys();
+
+            if (rs.next()) {
+                mesero.setIdMesero(rs.getInt(1));
+            } else {
+                JOptionPane.showMessageDialog(null,"No puedo obtener id");
+            }
+       
+        con.close();
+        }catch(SQLException e){
+        
+            JOptionPane.showMessageDialog(null,"Error al guardar Alumno");
+        }
+ }
+    
+    /**
+     *
+     */
+    public void borrarMesero(int idMesero){
+    
+     
+        try {
+            
+            String sql = "DELETE FROM mesero  WHERE idMesero=?;";
+            
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, idMesero);
+            ps.executeUpdate();
+            ps.close();
+        
+        } catch (SQLException ex) {
+            Logger.getLogger(MeseroData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+          JOptionPane.showMessageDialog(null,"Error al Borrar Mesero");
+          
+        }
 }
